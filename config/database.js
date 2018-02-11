a1bcd985777004d4f50f896353edb1cb574f7da3
DrGonzoIII <pacolove@gmail.com>
2018-02-11 19:13:16 +0100

    Se crea las variables de desarollo y produccion

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://admin:I[v<UoEmJk%g@ds129428.mlab.com:29428/tasklistddbb' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+  //mongoURI: 'mongodb://admin:I[v<UoEmJk%g@ds129428.mlab.com:29428/tasklistddbb',
+  mongoURI: process.env.MONGO_URI,
+  cookie-key: process.env.COOKIE_KEY
+
+}
